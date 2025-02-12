fc96e01ead5900e72f4c5d2a1bd9d724b7561825
kevinalvarez91 <kevinalvarezcampos@gmail.com>
2025-02-12 10:44:01 -0800

    updated with links and refactoring

--- final_project/src/pages/HomePage.jsx
+++ final_project/src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import Header from "./Header.jsx";
 
 export default function HomePage() {
   const navigate = useNavigate();
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+      <Header />
       <h1 className="text-4xl font-bold mb-6 text-gray-800">This is the Homepage</h1>
       <button 
         onClick={() => navigate("/")} 
-        className="p-2 bg-red-500 text-white rounded"
-      >
+        className="p-2 bg-red-500 text-white rounded">
         Logout
       </button>
     </div>
