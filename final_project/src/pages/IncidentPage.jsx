541dd8476d72ba0dfbc23442aaf71d1b1c58b0c8
Daniel <dsanchezcruz@ucla.edu>
2025-02-23 12:07:26 -0800

    Added the comment section to the incident page, needs styling and more functionality

--- final_project/src/pages/IncidentPage.jsx
+++ final_project/src/pages/IncidentPage.jsx
@@ -5,9 +5,17 @@ import "../css/IncidentPage.css";
 
 
 const IncidentPage = () => {
-  //for 
+  //for handling resolved -> will be used for the buttons that we will then add the bar for 
   const [isResolved, setIsResolved] = useState(0);
   const [isUnresolved, setIsUnresolved] = useState(0);
+  const [commentData,setCommentData]=useState(
+    {
+      commentText:"",
+      comments:[]
+    }
+  );
+
+
   const handleResolvedClick = () => {
     setIsResolved(true);
   }
@@ -18,11 +26,12 @@ const IncidentPage = () => {
     //including the responsiveAppBar
 
     //this is for the title 
-    <div>
+    <div className="incident-page">
     <ResponsiveAppBar />
     <div className= "title-container">
       <h1>Scooter Accident</h1>
       <h3 className="location">Location: Bruinwalk</h3>
+
       {/*this is for the description */}
       <p className="description">
         Description: I was just walking and saw someone get
@@ -39,7 +48,58 @@ const IncidentPage = () => {
         width="500" 
         className="mx-auto my-4 block"
         />
+      {/*this is for the comment section*/}
+      <div className="comment-section">
+        <p>
+          Roar Board
+        </p>
+        {/*for writing comments */}
+        <div className="comment-container">
+        <div className="comment-input">
+          <textarea
+            type="text" 
+            placeholder="type a thought..."
+            value={commentData.commentText}
+            onChange={(e) => {
+              setCommentData({...commentData,commentText: e.target.value});
+            }} 
+            
+          />
+          <button onClick={() => {
+            //stop any empty comments 
+            if( commentData.commentText.trim()==="") return;
+
+            setCommentData({
+              //keeping the old comments
+              ...commentData,
+              //adding the new comment
+              comments:[...commentData.comments,commentData.commentText],
+              //ckearing the comment input field
+              commentText:""
+            })
+          }}
+          >
+            Roar
+          </button>
+        </div>
+          {/*we now have a list of comments, time to display them*/}
+          <ul className="all-comments">
+            {commentData.comments.map((comment,index) => (
+              <li key={index} className="comment">
+               <textarea
+                readOnly
+                value={comment}
+                className="comment-box"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
+    
+    
+
     </div>
   
   );
